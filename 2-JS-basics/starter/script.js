31b5b49968578e31f66fcf053452c62306fe93eb
ayslepnev <ayslepnev@domclick.ru>
2018-10-22 09:39:24 +0300

    MASTER: Init

---
+++ 2-JS-basics/starter/script.js
@@ -0,0 +1,149 @@
+/*****************************
+* Variables and data types
+* */
+/*
+
+var firstName = 'John';
+console.log(firstName);
+
+var lastName = 'Smith';
+var age = 28;
+
+var fullAge = true;
+console.log(fullAge);
+
+var job;
+console.log(job);
+
+job = 'Teacher';
+console.log(job);
+
+
+var _3years = 3;
+var jognMark = 'Jogn and Mark';
+// var function = 23;
+// var delete = 23;
+*/
+
+
+/*****************************
+ * Variables and data types
+ * */
+/*
+var firstName = 'John';
+var age = 28;
+
+// Type coercion
+console.log(firstName + ' ' + age);
+
+var job, isMarried;
+job = 'Teacher';
+isMarried = false;
+
+console.log(firstName + '  is a ' + age + ' year old '
+  + job + ' Is he married? ' + isMarried);
+
+// Variable mutation
+age = 'twenty eight';
+job = 'drive';
+
+// alert(firstName + '  is a ' + age + ' year old '
+//   + job + ' Is he married? ' + isMarried);
+*/
+
+
+/*****************************
+ * Basic operators
+ * */
+/*
+var year, yearJohn, yearMark;
+now = 2018;
+ageJohn = 28;
+ageMark = 33;
+
+// Math operators
+yearJohn = now - 28;
+yearMark = now - 33;
+
+console.log(yearJohn);
+console.log(now + 2);
+console.log(now + 2);
+console.log(now / 10);
+
+
+// Logical operators
+var johnOlder = ageJogn < ageMark;
+console.log(johnOlder);
+
+
+// typeof operator
+console.log(typeof johnOlder);
+console.log(typeof ageJohn);
+console.log(typeof 'Mark is older the John');
+var x;
+console.log(typeof x);
+*/
+
+
+/*****************************
+ * Basic operators
+ * */
+/*
+
+var now = 2018;
+var yearJohn = 1989;
+var fullAge = 18;
+
+var isFullAge = now - yearJohn >= fullAge; // true
+console.log(isFullAge);
+
+var ageJogn = now - yearJohn;
+var ageMark = 35;
+var average = ageJogn + ageMark / 2;
+console.log(average);
+
+// Multiple assignments
+var x, y;
+x = y = (3 + 5) * 4 - 6; // 8 * 4 -6 // 32 -6
+console.log(x, y);
+
+// More operators
+x *= 2;
+console.log(x);
+x += 10;
+console.log(x);
+x++;
+console.log(x);
+*/
+
+
+/*****************************
+ * CODING CHALLENGE 1
+ * */
+/*
+
+var massMark = 76; // kg
+var heightMark = 1.69; // meters
+
+var massJohn = 92;
+var heightJohn = 1.95;
+
+var BMIMark = massMark / ( heightMark + heightMark);
+var BMIJohn = massJohn / ( heightJohn + heightJohn);
+console.log(BMIMark, BMIJohn);
+
+var markHigherBMI = BMIMark > BMIJohn;
+console.log('Is Mark\'s BMI higher than John\'s? ' + markHigherBMI);
+*/
+
+
+/*****************************
+ * If / else statements
+ * */
+
+var firstName = 'John';
+var civilStatus = 'single';
+
+// if ()
+
+
