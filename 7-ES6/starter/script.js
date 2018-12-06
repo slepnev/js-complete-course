f54724f103eff3c1276efb55c5988e0c1b0b5eda
ayslepnev <ayslepnev@domclick.ru>
2018-12-06 08:31:17 +0300

    JS-COMPLETE: ES6

--- 7-ES6/starter/script.js
+++ 7-ES6/starter/script.js
@@ -132,6 +132,7 @@ console.log(ages6);*/
 // Lecture: Arrow functions 2
 
 // ES5
+/*
 var box5 = {
   color: 'green',
   position: 1,
@@ -170,3 +171,151 @@ const box66 = {
   }
 };
 box66.clickMe();
+*/
+
+
+/*
+function Person(name) {
+  this.name = name;
+}
+
+// ES5
+Person.prototype.myFriends5 = function (friends) {
+  var arr = friends.map(function (el) {
+    return this.name + ' is friends with ' + el;
+  }.bind(this));
+
+  console.log(arr);
+};
+
+var friends = ['Bob', 'Jane', 'Mark'];
+new Person('John').myFriends5(friends);
+
+
+// ES6
+Person.prototype.myFriends6 = function (friends) {
+  var arr = friends.map(el => `${this.name} is friends with ${el}`);
+
+  console.log(arr);
+};
+
+new Person('Mike').myFriends6(friends);
+*/
+
+
+/////////////////////////////
+// Lecture: Destructuring
+/*
+// ES5
+var john = ['John', 26];
+// var name = john[0];
+// var age = john[1];
+
+// ES6
+const [name, age] = ['John', 26];
+console.log(name, age);
+
+const obj = {
+  firstName: 'John',
+  lastName: 'Smith'
+};
+
+const {firstName, lastName} = obj;
+console.log(firstName, lastName);
+
+const {firstName: a, lastName: b} = obj;
+console.log(a, b);
+
+
+function calcAgeRetirement(year) {
+  const age = new Date().getFullYear() - year;
+  return [age, 65 - age];
+}
+
+const [age, retirement] = calcAgeRetirement(1990);
+console.log(age, retirement);
+*/
+
+/////////////////////////////
+// Lecture: Array
+/*
+const boxes = document.querySelectorAll('.box');
+
+// ES5
+var boxesArr5 = Array.prototype.slice.call(boxes);
+boxesArr5.forEach(function (cur) {
+  cur.style.backgroundColor = 'dodgerblue';
+});
+
+// ES6
+const boxesArr6 = Array.from(boxes);
+boxesArr6.forEach(cur => cur.style.backgroundColor = 'dodgerblue');
+
+// ES5
+for (var i = 0; i < boxesArr5.length; i++) {
+
+  if (boxesArr5[i].className === 'box blue') {
+    continue;
+  }
+
+  boxesArr5[i].textContent = 'I changed to blue';
+
+}
+
+// ES6
+for (const cur of boxesArr6) {
+  if (cur.className.includes('blue')) {
+    continue;
+  }
+  cur.textContent = 'I changed to blue!';
+}
+
+
+// ES5
+var ages = [12, 17, 8, 21, 14, 11];
+
+var full = ages.map(function (cur) {
+  return cur >= 18;
+});
+console.log(full);
+
+console.log(full.indexOf(true));
+console.log(ages[full.indexOf(true)]);
+
+// ES6
+console.log(ages.findIndex(cur => cur >= 18));
+console.log(ages.find(cur => cur >= 18));
+*/
+
+
+/////////////////////////////
+// Lecture: Spread operator
+/*
+function addFourAges(a, b, c, d) {
+  return a + b + c + d;
+}
+
+var sum1  = addFourAges(18, 30, 12, 21);
+console.log(sum1);
+
+// ES5
+var ages = [18, 30, 12, 21];
+var sum2 = addFourAges.apply(null, ages);
+console.log(sum2);
+
+// ES6
+const sum3 = addFourAges(...ages);
+console.log(sum3);
+
+
+const familySmith = ['John', 'Jane', 'Mark'];
+const familyMiller = ['Mary', 'Bob', 'Ann'];
+const bigFamily = [...familySmith, ...familyMiller];
+console.log(bigFamily);
+
+const h = document.querySelector('h1');
+const boxes = document.querySelectorAll('.box');
+const all = [h, ...boxes];
+
+Array.from(all).forEach(cur => cur.style.color = 'purple');
+*/
