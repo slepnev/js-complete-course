8e43fedc0526b0037ca6d2819f1b880f53183c31
ayslepnev <ayslepnev@domclick.ru>
2018-12-24 08:34:22 +0300

    JS-COMPLETE: MVC

--- 9-forkify/starter/src/js/index.js
+++ 9-forkify/starter/src/js/index.js
@@ -109,6 +109,27 @@ const controlRecipe = async () => {
 // window.addEventListener('load', controlRecipe);
 ['hashchange', 'load'].forEach(event => window.addEventListener(event, controlRecipe));
 
+// Handling recipe button clicks
+elements.recipe.addEventListener('click', e => {
+  if (e.target.matches('.btn-decrease, .btn-decrease *')) {
+    // Decrease button is click
+    if (state.recipe.servings > 1) {
+      state.recipe.updateServings('dec');
+      recipeView.updateServingsIngredients(state.recipe);
+    }
+  } else if (e.target.matches('.btn-increase, .btn-increase *')) {
+    // Increase button is click
+    state.recipe.updateServings('inc');
+    recipeView.updateServingsIngredients(state.recipe);
+  } else if (e.target.matches('.recipe__btn--add, .recipe__btn--add *')) {
+    // Add ingredients the shopping list
+    controlList();
+  } else if (e.target.matches('.recipe__love, .recipe__love *')) {
+    // Like controller
+    controlLike();
+  }
+});
+
 
 /*
 * LIST CONTROLLER
@@ -147,7 +168,6 @@ elements.shopping.addEventListener('click', e => {
 /*
 * LIKES CONTROLLER
 * */
-state.likes = new Likes();
 const controlLike = () => {
   if (!state.likes) state.likes = new Likes();
   const currentID = state.recipe.id;
@@ -166,7 +186,7 @@ const controlLike = () => {
     likesView.toggleLikeBtn(true);
 
     // Add like to UI list
-    console.log(state.likes);
+    likesView.renderLike(newLike);
 
   // User HAS liked current recipe
   } else {
@@ -177,30 +197,21 @@ const controlLike = () => {
     likesView.toggleLikeBtn(false);
 
     // Remove like from UI list
-    console.log(state.likes);
+    likesView.deleteLike(currentID);
   }
+  likesView.toggleLikeMenu(state.likes.getNumLikes());
 };
 
+// Restore liked recipes on page load
+window.addEventListener('load', () => {
+  state.likes = new Likes();
 
-// Handling recipe button clicks
-elements.recipe.addEventListener('click', e => {
-  if (e.target.matches('.btn-decrease, .btn-decrease *')) {
-    // Decrease button is click
-    if (state.recipe.servings > 1) {
-      state.recipe.updateServings('dec');
-      recipeView.updateServingsIngredients(state.recipe);
-    }
-  } else if (e.target.matches('.btn-increase, .btn-increase *')) {
-    // Increase button is click
-    state.recipe.updateServings('inc');
-    recipeView.updateServingsIngredients(state.recipe);
-  } else if (e.target.matches('.recipe__btn--add, .recipe__btn--add *')) {
-    // Add ingredients the shopping list
-    controlList();
-  } else if (e.target.matches('.recipe__love, .recipe__love *')) {
-    // Like controller
-    controlLike();
-  }
+  // Restore likes
+  state.likes.readStorage();
+
+  // Toggle like menu button
+  likesView.toggleLikeMenu(state.likes.getNumLikes());
+
+  // Render the existings likes
+  state.likes.likes.forEach(like => likesView.renderLike(like));
 });
-
-window.l = new List();
